2d4e2c13b09195d5c43cc5e5ded5883bed5d67f2
Rhys Bratti <rhysbratti@gmail.com>
2024-12-14 14:04:35 -0500

    Updating docker file and docker compose files

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,12 +1,6 @@
 #!/bin/sh
 
-echo "Parsing command"
-if [ $# -gt 0 ]; then
-    echo "Running with command: $@"
-    exec /app/auth-server "$@"
-else
-    echo "Running diesel migrations"
-    /usr/local/cargo/bin/diesel database setup
-    echo "Running application"
-    exec /app/auth-server
-fi
+echo "Running diesel migrations"
+/usr/local/cargo/bin/diesel database setup
+echo "Running application"
+exec /app/auth-server
